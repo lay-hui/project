c3efcd64800e294172940089bac9de3637d1ce0f
小辉 <1652016835@qq.com>
2020-07-21 14:11:20 +0800

    project

---
+++ kmall/home/src/pages/common/index.js
@@ -0,0 +1,8 @@
+/*
+* @Author: TomChen
+* @Date:   2019-08-21 17:44:11
+* @Last Modified by:   TomChen
+* @Last Modified time: 2019-08-22 10:06:20
+*/
+require('node_modules/font-awesome/css/font-awesome.min.css')
+require('./index.css')
